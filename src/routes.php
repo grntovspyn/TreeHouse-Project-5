d3a43c832faafc99713cc728a1329b9d076813bb
grntovspyn <grntovspyn@gmail.com>
2019-12-30 22:39:17 -0800

    FinaL

--- src/routes.php
+++ src/routes.php
@@ -16,23 +16,23 @@ use App\WidgetController;
 
 $app->get('/', function ($request, $response, $args){
 
-//Pull at posts and tags
+//Pull all posts and tags and order by date
 $post = BlogPost\Post::with('tags')->orderBy('date', 'desc')->get();
 
-     // CSRF token name and value
- $csrf = $this->get('csrf');
- $nameKey = $csrf->getTokenNameKey();
- $valueKey = $csrf->getTokenValueKey();
- $csrf = [
-     $nameKey => $request->getAttribute($nameKey),
-     $valueKey => $request->getAttribute($valueKey)
-   ];
 
+// CSRF token name and value
+$csrf = $this->get('csrf');
+$nameKey = $csrf->getTokenNameKey();
+$valueKey = $csrf->getTokenValueKey();
+$csrf = [
+    $nameKey => $request->getAttribute($nameKey),
+    $valueKey => $request->getAttribute($valueKey)
+  ];
 
- return $this->view->render($response, "index.twig", [
-     'csrf' => $csrf,
-     'args' => $args,
-     'post' => $post,
+return $this->view->render($response, "index.twig", [
+    'csrf' => $csrf,
+    'args' => $args,
+    'post' => $post,
    
  ]);
 
@@ -41,26 +41,31 @@ $post = BlogPost\Post::with('tags')->orderBy('date', 'desc')->get();
 
 /************** DETAIL PAGE ROUTES ****************/
 
-$app->map(['GET', 'POST'],'/detail/[{id}]', function ($request, $response, $args) {
-      // $this->logger->addInfo("Detail Edit");
-      //pull post
-      $post = BlogPost\Post::find($args['id']);
- 
+$app->map(['GET', 'POST'],'/detail/{slug}', function ($request, $response, $args) {
+    $post = $specTags = $comments = "";
+    $this->logger->addInfo("Detail Page");
+      
 
-      $specTags = $post->tags;
-
-
-      $comments = $post->comments;
+    // Retrieve Post by Slug
+    $post = BlogPost\Post::where('slug', '=', $args['slug'])->first();
+    // Retrieve Tags
+    $specTags = $post->tags;
+    $comments = $post->comments;
 
 
       if($request->getMethod() == "POST") {
         $args = array_merge($args, $request->getParsedBody());
-        
-        //var_dump($post);
-        
-        $create = BlogPost\Comment::create(['post_id' => $args['id'], 'name' => $args['name'],'date' => date('d-m-Y') , 'body' => $args['comment']]);
+        $postId = $post->id;
+
+        try {
+            $create = BlogPost\Comment::create(['post_id' => $postId, 'name' => $args['name'], 'date' => date('d-m-Y'), 'body' => $args['comment']]);
+        } catch(Exception $e) {
+          echo "Unable to create new post" . $e->getMessage();
+          die();
+        }
+
         $basePath = $request->getUri()->getBasePath();
-       return $response->withStatus(302)->withHeader('Location', $basePath . '/detail/' . $args['id']);
+       return $response->withStatus(302)->withHeader('Location', $basePath . '/detail/' . $args['slug']);
       }
 
 
@@ -85,35 +90,46 @@ $app->map(['GET', 'POST'],'/detail/[{id}]', function ($request, $response, $args
 
 });
 
-
-$app->map(['GET', 'POST'],'/edit/[{id}]', function ($request, $response, $args) {
-  // CSRF token name and value
-  $csrf = $this->get('csrf');
-  $nameKey = $csrf->getTokenNameKey();
-  $valueKey = $csrf->getTokenValueKey();
-  $csrf = [
-      $nameKey => $request->getAttribute($nameKey),
-      $valueKey => $request->getAttribute($valueKey),
-  ];
+/****************** EDIT PAGE ROUTE **************************/
+$app->map(['GET', 'POST'],'/edit/[{slug}]', function ($request, $response, $args) {
+  $slug = "";
+  $this->logger->addInfo("Edit Page");
   
-  $post = BlogPost\Post::find($args['id']);
+  
+  
+  $post = BlogPost\Post::where('slug', $args['slug'])->first();
   $specTags = $post->tags;
 
   $basePath = $request->getUri()->getBasePath();
 //Edit post
   if($request->getMethod() == "POST") {
 
-    if(isset($_POST['delete'])) {
-      $post->tags()->detach($args['id']);
-      $post->delete($args['id']);
-      return $response->withStatus(302)->withHeader('Location', $basePath . '/');
+
+
+    try {
+        if (isset($_POST['delete'])) {
+            $post->tags()->detach($args['id']);
+            $post->delete($args['id']);
+
+            return $response->withStatus(302)->withHeader('Location', $basePath.'/');
+        }
+    } catch(Exception $e){
+      echo "Unable to delete post" . $e->getMessage();
+      die();
     }
 
 
     $args = array_merge($args, $request->getParsedBody());
+    $postId = $post->id;
     $args = $post->sanitize($args);
     $slug = $post->slugify($args['title']);
-    $post->where('id', $args['id'])->update(['title' => $args['title'],'date' => $args['date'] , 'body' => $args['body'], 'slug' => $args['body']]);
+
+
+    try {
+        $post->where('id', $postId)->update(['title' => $args['title'], 'date' => $args['date'], 'body' => $args['body'], 'slug' => $slug]);
+    }catch(Exception $e) {
+      echo "Unable to update post" . $e->getMessage();
+    }
 
     if ('' != $args['tags']) {
         $newTags = explode(',', (str_replace(' ', '', $args['tags'])));
@@ -124,23 +140,36 @@ $app->map(['GET', 'POST'],'/edit/[{id}]', function ($request, $response, $args) 
     
    
     if($newTags !== NULL){
-    foreach ($newTags as $value) {
-        //$filteredValue = $post->sanitize($value);
-        $tag = new BlogPost\Tag(['tags' => $value]);
-        $id = $post->tags()->save($tag)->id;
-        
-        $arr[] = $id;
-    }
-    $post->tags()->sync($arr);
+      try{
+
+      foreach ($newTags as $value) {
+        $value = filter_var($value, FILTER_SANITIZE_STRING);
+          $tag = new BlogPost\Tag(['tags' => $value]);
+          $id = $post->tags()->save($tag)->id;
+          
+          $arr[] = $id;
+      }
+      $post->tags()->sync($arr);
+  }catch(Exception $e) {
+    echo "Unable to update tags" . $e->getMessage();
+    die();
+  } 
   } else {
     $post->tags()->detach();
+} 
+
+   return $response->withStatus(302)->withHeader('Location', $basePath . '/');
+
   }
-    
 
-    return $response->withStatus(302)->withHeader('Location', $basePath . '/');
-
-  }
-
+  // CSRF token name and value
+  $csrf = $this->get('csrf');
+  $nameKey = $csrf->getTokenNameKey();
+  $valueKey = $csrf->getTokenValueKey();
+  $csrf = [
+      $nameKey => $request->getAttribute($nameKey),
+      $valueKey => $request->getAttribute($valueKey),
+  ];
  
   return $this->view->render($response, 'edit.twig', [
       'csrf' => $csrf,
@@ -150,15 +179,13 @@ $app->map(['GET', 'POST'],'/edit/[{id}]', function ($request, $response, $args) 
   ]);
 })->setName('edit');
 
-$app->map(['GET', 'POST'],'/new', function ($request, $response, $args) {
-  // CSRF token name and value
-  $csrf = $this->get('csrf');
-  $nameKey = $csrf->getTokenNameKey();
-  $valueKey = $csrf->getTokenValueKey();
-  $csrf = [
-      $nameKey => $request->getAttribute($nameKey),
-      $valueKey => $request->getAttribute($valueKey),
-  ];
+
+
+
+/****************** CREATE POST ROUTE **************************/
+
+$app->map(['GET', 'POST'],'/newpost', function ($request, $response, $args) {
+  $this->logger->addInfo("New Post Page");
   
 
 $post = new BlogPost\Post;
@@ -170,7 +197,12 @@ $post = new BlogPost\Post;
     $args = array_merge($args, $request->getParsedBody());
     $args = $post->sanitize($args);
     $slug = $post->slugify($args['title']);
-    $postId = $post->create(['title' => $args['title'],'date' => $args['date'] , 'body' => $args['body'], 'slug' => $slug])->id;
+
+    try {
+        $postId = $post->create(['title' => $args['title'], 'date' => $args['date'], 'body' => $args['body'], 'slug' => $slug])->id;
+    } catch(Exception $e) {
+      echo "Unable to create new post" . $e->getMessage();
+    }
 
     if ('' != $args['tags']) {
         $newTags = explode(',', (str_replace(' ', '', $args['tags'])));
@@ -180,27 +212,38 @@ $post = new BlogPost\Post;
     $arr = array();
     
    
-    if($newTags !== NULL){
-    foreach ($newTags as $value) {
-        $filteredValue = $post->sanitize($value);
-        $post = BlogPost\Post::find($postId);
-        $tag = new BlogPost\Tag(['tags' => $value]);
-       
-        $id = $post->tags()->save($tag)->id;
-        
-        $arr[] = $id;
+    if (null !== $newTags) {
+        try {
+            foreach ($newTags as $value) {
+              $value = filter_var($value, FILTER_SANITIZE_STRING);
+                $post = BlogPost\Post::find($postId);
+                $tag = new BlogPost\Tag(['tags' => $value]);
+
+                $id = $post->tags()->save($tag)->id;
+
+                $arr[] = $id;
+            }
+            $post->tags()->sync($arr);
+        }catch (Exception $e) {
+          echo "Unable to create new tags with new post" . $e->getMessage();
+          die();
+       }
     }
-    $post->tags()->sync($arr);
-  }
-    
     return $response->withStatus(302)->withHeader('Location', $basePath . '/');
 
   }
 
- 
+  // CSRF token name and value
+  $csrf = $this->get('csrf');
+  $nameKey = $csrf->getTokenNameKey();
+  $valueKey = $csrf->getTokenValueKey();
+  $csrf = [
+      $nameKey => $request->getAttribute($nameKey),
+      $valueKey => $request->getAttribute($valueKey),
+  ];
   return $this->view->render($response, 'new.twig', [
       'csrf' => $csrf,
       'args' => $args,
       'post' => $post,
   ]);
-})->setName('new');
+})->setName('newpost');
